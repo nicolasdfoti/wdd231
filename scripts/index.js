5233ec02c250bdcfbb3dc1ca0317851dad4f269f
nicolasdfoti <nicolasdfoti@gmail.com>
2025-03-03 01:34:21 -0300

    Finished!

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 const currentYear = document.querySelector("#currentYear");
 const lastUpdated = document.querySelector("#lastUpdated");
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
 const today = new Date();
 
 currentYear.innerHTML = `&copy;${today.getFullYear()} - Nicolás Foti`;
 lastUpdated.innerHTML = `Last Modified: ${document.lastModified}`;
+
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("open");
+    navigation.classList.toggle("open");
+})
