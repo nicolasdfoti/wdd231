daa67d099d23ea3684b170239467e1bd532ccacf
nicolasdfoti <nicolasdfoti@gmail.com>
2025-03-03 01:07:11 -0300

    Finished!

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,5 @@ const currentYear = document.querySelector("#currentYear");
 const lastUpdated = document.querySelector("#lastUpdated");
 const today = new Date();
 
-currentYear.innerHTML = `&copy; ${today.getFullYear()} - Nicolás Foti`;
+currentYear.innerHTML = `&copy;${today.getFullYear()} - Nicolás Foti`;
 lastUpdated.innerHTML = `Last Modified: ${document.lastModified}`;
