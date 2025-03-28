f067fca328f68b730d65c20044fedd6e1897a18b
nicolasdfoti <nicolasdfoti@gmail.com>
2025-03-28 00:00:47 -0300

    Finished Modals

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -17,3 +17,78 @@ hamButton.addEventListener("click", () => {
 document.getElementById("timestamp").value = new Date().toISOString();
 
 // Membership Levels
+const membership = 'data/membership.json';
+const goldMember = document.querySelector("#gold");
+const silverMember = document.querySelector("#silver");
+const bronzeMember = document.querySelector("#bronze");
+const npMember = document.querySelector("#np-member");
+const membershipDetails = document.querySelector("#membership-details");
+
+async function getMemberInfo() {
+
+    const response = await fetch(membership);
+    const data = await response.json();
+
+    return data.levels;
+}
+
+displayMembershipDetails = (level) => {
+
+    membershipDetails.innerHTML = "";
+
+    const title = document.createElement("h1");
+    const description = document.createElement("p");
+    const publicity = document.createElement("p");
+    const benefits = document.createElement("ul");
+    const memberButton = document.createElement("button");
+    memberButton.classList.add("close-button");
+
+    title.innerHTML = level.title;
+    description.innerHTML = level.description;
+    publicity.innerHTML = `Publicity: ${level.publicity}`;
+
+    level.benefits.forEach(benefit => {
+        const benefitLi = document.createElement("li");
+        benefitLi.innerHTML = benefit;
+        benefits.appendChild(benefitLi);
+    })
+
+    memberButton.innerHTML = "X"
+
+    membershipDetails.appendChild(title);
+    membershipDetails.appendChild(description);
+    membershipDetails.appendChild(publicity);
+    membershipDetails.appendChild(benefits);
+    membershipDetails.appendChild(memberButton);
+    
+    memberButton.addEventListener("click", () => {
+        membershipDetails.close();
+    })
+
+    membershipDetails.showModal();
+
+}
+
+goldMember.addEventListener("click", async () => {
+    const levels = await getMemberInfo();
+    const goldLevel = levels.find(level => level.title === "Gold");
+    displayMembershipDetails(goldLevel);
+})
+
+silverMember.addEventListener("click", async () => {
+    const levels = await getMemberInfo();
+    const silverLevel = levels.find(level => level.title === "Silver");
+    displayMembershipDetails(silverLevel);
+})
+
+bronzeMember.addEventListener("click", async () => {
+    const levels = await getMemberInfo();
+    const bronzeLevel = levels.find(level => level.title === "Bronze");
+    displayMembershipDetails(bronzeLevel);
+})
+
+npMember.addEventListener("click", async () => {
+    const levels = await getMemberInfo();
+    const npLevel = levels.find(level => level.title === "NP (Non-Profit)");
+    displayMembershipDetails(npLevel);
+})
