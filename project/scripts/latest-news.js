596c1f496f4d828ec851d14bd9a74e674442f2c2
nicolasdfoti <nicolasdfoti@gmail.com>
2025-04-06 22:59:26 -0300

    Finished!

--- project/scripts/latest-news.js
+++ project/scripts/latest-news.js
@@ -8,3 +8,68 @@ hamButton.addEventListener("click", () => {
     hamButton.classList.toggle("open");
 })
 
+// News Section
+
+const sectionsContainer = document.querySelector("#latest-sections");
+const jsonFile = 'data/news.json';
+
+async function getNewsData() {
+    const response = await fetch(jsonFile);
+    const data = await response.json();
+    displayNewsInfo(data);
+    console.log(data);
+}
+
+function displayNewsInfo(data) {
+
+    data.forEach(newsItem => {
+
+        // Section Items
+        const section = document.createElement("section");
+        const title = document.createElement("h2");
+        const image = document.createElement("img");
+        const button = document.createElement("button");
+    
+        title.innerHTML = newsItem.title;
+        image.setAttribute("src", newsItem.image);
+        image.setAttribute("width", 300);
+        image.setAttribute("loading", "lazy");
+        button.innerHTML = "Read more here!";
+    
+        section.appendChild(title);
+        section.appendChild(image);
+        section.appendChild(button);
+        sectionsContainer.appendChild(section);
+
+        // Dialog Items
+        const dialogContainer = document.createElement("dialog");
+        const dialogTitle = document.createElement("h2");
+        const dialogImage = document.createElement("img");
+        const dialogDescription = document.createElement("p");
+        const dialogButton = document.createElement("button");
+        dialogButton.classList.add("close-button");
+
+        dialogTitle.innerHTML = newsItem.title;
+        dialogImage.setAttribute("src", newsItem.image)
+        dialogDescription.innerHTML = newsItem.description;
+        dialogButton.innerHTML = "Close";
+
+        dialogContainer.appendChild(dialogTitle);
+        dialogContainer.appendChild(dialogImage);
+        dialogContainer.appendChild(dialogDescription);
+        dialogContainer.appendChild(dialogButton);
+
+        sectionsContainer.appendChild(dialogContainer);
+
+        button.addEventListener("click", () => {
+            dialogContainer.showModal();
+        })
+
+        dialogButton.addEventListener("click", () => {
+            dialogContainer.close();
+        })
+    })
+    
+}
+
+getNewsData();
---
+++ project/scripts/thankyou.js
@@ -0,0 +1,32 @@
+// Toggle Nav Button
+
+const hamButton = document.querySelector("#hamburgerButton");
+const nav = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    hamButton.classList.toggle("open");
+})
+
+
+// Get User info
+
+const thankYouContainer = document.querySelector("#thankyou-div");
+const myInfo = new URLSearchParams(window.location.search);
+
+thankYouContainer.innerHTML = `
+    <h1>Thank you, ${myInfo.get("firstname")}!</h2>
+
+    <div id="thankyou-info">
+
+        <h2>Your information:</h2>
+        <p>Your name: ${myInfo.get("firstname")} | Last name: ${myInfo.get("lastname")}</p>
+
+        <h2>Your contact information:</h2>
+        <p>Your email: ${myInfo.get("email")} | Phone: ${myInfo.get("phone")}</p>
+
+        <h2>Relevant Information</h2>
+        <p>Your preferred communication method: ${myInfo.get("communication")} | Your improvements: ${myInfo.get("improvements")}</p>
+
+    </div>
+`;
